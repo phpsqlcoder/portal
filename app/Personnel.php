48ecd228906aa1f0605f92d394a8b6432e17d60e
unknown <dexterdev0320@gmail.com>
2020-09-07 13:57:06 +0800

    Scheduler and Personnel Profile - BETA

--- app/Personnel.php
+++ app/Personnel.php
@@ -8,6 +8,11 @@ class Personnel extends Model
 {
     protected $guarded = [];
 
+    public function getIsActiveAttribute($status)
+    {
+        return ($status == 1) ? 'Active' : 'Inactive';
+    }
+
     public function schedules()
     {
         return $this->belongsToMany('App\Schedule');
